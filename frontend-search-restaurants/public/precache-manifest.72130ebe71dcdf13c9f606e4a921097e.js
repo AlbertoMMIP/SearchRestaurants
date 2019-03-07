9a7c7aee82ce4c3eda843b856e47cc75c250e4b5
Alberto Martinez <josealberto.martinezmarquez@gmail.com>
2019-03-07 11:28:20 -0600

    Deploy

---
+++ frontend-search-restaurants/public/precache-manifest.72130ebe71dcdf13c9f606e4a921097e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "49bd20d74f3b1183cdf5",
+    "url": "/static/js/main.53874f62.chunk.js"
+  },
+  {
+    "revision": "fe925b75b011eab74577",
+    "url": "/static/js/2.604b70c3.chunk.js"
+  },
+  {
+    "revision": "49bd20d74f3b1183cdf5",
+    "url": "/static/css/main.be194f3f.chunk.css"
+  },
+  {
+    "revision": "fe925b75b011eab74577",
+    "url": "/static/css/2.725f271c.chunk.css"
+  },
+  {
+    "revision": "9705b12f66516efe7a93447f2d125bfa",
+    "url": "/index.html"
+  }
+];
