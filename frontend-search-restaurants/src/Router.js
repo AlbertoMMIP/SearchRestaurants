628e3d8e6b2813a0942553ebd56f4bf9904813d5
Alberto Martinez <out_jamartinez@independencia.com.mx>
2020-06-01 17:52:18 -0500

    Fix mistake

--- frontend-search-restaurants/src/Router.js
+++ frontend-search-restaurants/src/Router.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import {Switch,Route} from 'react-router-dom';
+import {Switch, BrowserRouter, Route} from 'react-router-dom';
 import { Home } from './components/home/Home';
 
 export const Router = () => (
-    <Switch>
-        <Route exact path='/' component={Home}  />
-    </Switch>
+    <BrowserRouter>
+        <Switch>
+            <Route exact path='/' component={Home}  />
+        </Switch>
+    </BrowserRouter>
+
 );
